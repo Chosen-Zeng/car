aa3c3bc4594b274619373c6cb73031a82d24875f
Chosen_Zeng <1779730593@qq.com>
2025-11-10 16:46:33 +0800

    [del] delete redundant section and related code - rename toolchain folder

--- cmake/ATfE.cmake
+++ cmake/ATfE.cmake
@@ -15,7 +15,7 @@ set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # MCU specific flags
-set(TARGET_FLAGS "--target=thumbv7m-unknown-none-eabi -mfpu=none -fno-exceptions -fno-rtti -munaligned-access -march=armv7-m -mcpu=cortex-m3 -mthumb -fshort-enums")
+set(TARGET_FLAGS "--target=thumbv7m-unknown-none-eabi -mfpu=none -fno-exceptions -fno-rtti -munaligned-access -march=armv7-m -mcpu=cortex-m3 -mthumb -fshort-enums -fno-math-errno")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -MP")
@@ -26,7 +26,7 @@ set(CMAKE_C_FLAGS_RELEASE "-Os -g0")
 
 set(CMAKE_EXE_LINKER_FLAGS "${TARGET_FLAGS}")
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lcrt0-minimal")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lcrt0-none")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T \"${CMAKE_SOURCE_DIR}/STM32F103XX_FLASH.ld\"")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections")
